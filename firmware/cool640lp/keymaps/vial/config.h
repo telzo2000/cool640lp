e3701e0fb9c7a3e777beaf7fa41a54ed7fa55bdc
木村 光輝 <67196597+telzo2000@users.noreply.github.com>
2024-10-08 16:22:07 +0900

    fix

---
+++ firmware/cool640lp/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+// Copyright 2023 m.ki (@telzo2000)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0x74, 0x04, 0x4C, 0xCB, 0x41, 0x02, 0x25, 0xF6}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
